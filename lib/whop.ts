0840d7c55d76a159a895517691ceb81e05b0f6e8
maxostaficiuc-cell <max.ostaficiuc@gmail.com>
2026-08-24 11:41:33 +0300

    whop fix 3

--- lib/whop.ts
+++ lib/whop.ts
@@ -1,9 +1,9 @@
-import { Whop } from "@whop/sdk";
+import { WhopClient } from "@whop/sdk";
 
 function createWhopClient() {
   const sandbox = process.env.WHOP_SANDBOX === "true";
-  return new Whop({
-    apiKey: process.env.WHOP_COMPANY_API_KEY!,
+  return new WhopClient({
+    token: process.env.WHOP_COMPANY_API_KEY!,
     ...(process.env.WHOP_WEBHOOK_SECRET && {
       webhookKey: Buffer.from(process.env.WHOP_WEBHOOK_SECRET).toString("base64"),
     }),
